a21ecdf627257cda9ba804f19cd11908fcefc781
Naseem Hamed <naseemhamed100@gmail.com>
2020-10-21 13:51:21 -0400

    Added build files to repo

---
+++ build/precache-manifest.a13cc33c3e1034c23325f2e8c67e222e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de051011a8b75d52e8939303041c1412",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f1b08dcad1b986bc7b4",
+    "url": "/static/css/2.52df8c41.chunk.css"
+  },
+  {
+    "revision": "7ea024d202f9acb3387a",
+    "url": "/static/css/main.5dca982b.chunk.css"
+  },
+  {
+    "revision": "8f1b08dcad1b986bc7b4",
+    "url": "/static/js/2.77042944.chunk.js"
+  },
+  {
+    "revision": "7b68d577a51a453fc0ba0ae238c66fdf",
+    "url": "/static/js/2.77042944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea024d202f9acb3387a",
+    "url": "/static/js/main.e099f6a5.chunk.js"
+  },
+  {
+    "revision": "915be551893e47e9bf3e",
+    "url": "/static/js/runtime-main.e250a274.js"
+  },
+  {
+    "revision": "d462267262eb85b8dcc6321f10a2830f",
+    "url": "/static/media/CyberSafetyManual_SK2.d4622672.pdf"
+  },
+  {
+    "revision": "a3e97a26478707d891f5025e84ea3cde",
+    "url": "/static/media/cams.a3e97a26.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1497711902aece84c7a652a0a9dd4d89",
+    "url": "/static/media/mit-white.14977119.jpeg"
+  }
+]);
